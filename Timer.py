3e2085eb7fb96558526388ff4139f36c87cdf320
孙培根 <pgsunbacon@gmail.com>
2019-06-23 18:13:32 +0800

    Using closure to combine the func decorator and the class decorator

--- Timer.py
+++ Timer.py
@@ -6,30 +6,19 @@
 import time
 import inspect
 
-from functools import wraps
+from functools import wraps, partial
 
+class NoInstances(type):
+    def __call__(self, *args, **kwargs):
+        raise TypeError("Can't instantiate directly")
 
-class Timer:
+class Timer(metaclass=NoInstances):
     times = {}
 
-    def __init__(self, obj):
-
-        self.obj = obj
-        if inspect.isfunction(self.obj):
-            self.obj_type = 'function'
-        if inspect.isclass(self.obj):
-            self.obj_type = 'class'
-
-    def __call__(self, *args, **kw):
-        if self.obj_type == 'function':
-            Timer.time_this(self.obj, *args, **kw)
-        elif self.obj_type == 'class':
-            print(123)
-
     @staticmethod
-    def time_this(func, *args, **kw):
+    def time_this(func, *args, **kwargs):
         start_time = time.time()
-        func_ret = func(*args, **kw)
+        func_ret = func(*args, **kwargs)
         duration = time.time() - start_time
         if func.__name__ in Timer.times.keys():
             Timer.times[func.__name__] = [Timer.times[func.__name__][0] + duration,
@@ -64,16 +53,62 @@ class Timer:
                   i[0])
         print("Total time is: {}s.".format(round(Timer.times["__total_time__"][0], 2)))
 
+def timer(obj):
+    if inspect.isfunction(obj):
+        # obj_type = 'function'
+        @wraps(obj)
+        def wrapper(*args, **kwargs):
+            return Timer.time_this(obj, *args, **kwargs)
+        return wrapper
+    elif inspect.isclass(obj):
+        # obj_type = 'class'
+        orig_getattribute = obj.__getattribute__
+        def new_getattribute(self, name):
+            x = orig_getattribute(self, name)
+            # print(x.__name__)
+            if callable(x):
+                return partial(Timer.time_this, x)
+            else:
+                return x
+        obj.__getattribute__ = new_getattribute
+        return obj
+    else:
+        raise Exception("Invalid Decorate type!", obj)
 
-@Timer
+
+@timer
 def tmp():
     time.sleep(1)
     print(11111)
 
 
-@Timer
+@timer
 def tmp1():
     time.sleep(2)
+
+class Tester1:
+    @timer
+    def sleeptest1(self):
+        time.sleep(2)
+
+    @staticmethod
+    @timer
+    def sleeptest2():
+        time.sleep(0.5)
+
+@timer
+class Tester2:
+    def __init__(self):
+        print("Initializing...")
+        super().__init__()
+        self.x = 0
+
+    def sleeptest1(self):
+        time.sleep(1)
+
+    @staticmethod
+    def sleeptest2():
+        time.sleep(2)
 
 
 if __name__ == "__main__":
@@ -81,5 +116,13 @@ if __name__ == "__main__":
     tmp()
     tmp1()
     tmp1()
+
+    t1 = Tester1()
+    t1.sleeptest1()
+    Tester1.sleeptest2()
+
+    t2 = Tester2()
+    t2.sleeptest1()
+    Tester2.sleeptest2()
 
     Timer.show()
